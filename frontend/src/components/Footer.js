1a2cd720a786f6404f3dd996ae7e150f606e0387
Ajang-Deng98 <a.deng3@alustudent.com>
2025-08-21 13:28:03 +0200

    "updates"

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -90,7 +90,7 @@ const Footer = () => {
         
         <div className="footer-bottom">
           <div className="footer-bottom-content">
-            <p>&copy; 2024 Abbaswhispers. All rights reserved.</p>
+            <p>&copy; 2025 Abbaswhispers. All rights reserved.</p>
             <div className="footer-legal">
               <a href="/privacy">Privacy Policy</a>
               <a href="/terms">Terms of Service</a>
