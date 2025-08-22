f1f2006807be36fec2bac50997795d0e4935515d
Ajang-Deng98 <a.deng3@alustudent.com>
2025-08-22 11:59:20 +0200

    cleanup - restore original components only

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-route
 import { motion } from 'framer-motion';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import MinimalHome from './pages/MinimalHome';
+import Home from './pages/Home';
 import About from './pages/About';
 import Volumes from './pages/Volumes';
-import MinimalBlog from './pages/MinimalBlog';
+import Blog from './pages/Blog';
 import BlogPost from './pages/BlogPost';
 import Contact from './pages/Contact';
 import PrayerRequest from './pages/PrayerRequest';
@@ -33,10 +33,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout><MinimalHome /></Layout>} />
+        <Route path="/" element={<Layout><Home /></Layout>} />
         <Route path="/about" element={<Layout><About /></Layout>} />
         <Route path="/volumes" element={<Layout><Volumes /></Layout>} />
-        <Route path="/blog" element={<Layout><MinimalBlog /></Layout>} />
+        <Route path="/blog" element={<Layout><Blog /></Layout>} />
         <Route path="/blog/:id" element={<Layout><BlogPost /></Layout>} />
         <Route path="/contact" element={<Layout><Contact /></Layout>} />
         <Route path="/prayer-request" element={<Layout><PrayerRequest /></Layout>} />
