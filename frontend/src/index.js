7f4dcc3d754901c49ec0127521a0880db30d308f
Ajang-Deng98 <a.deng3@alustudent.com>
2025-09-11 16:56:05 +0200

    Emergency fix: Add debug mode and static test page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 
 import App from './App';
+import DebugApp from './DebugApp';
+
+// Use debug app if there are issues
+const AppToRender = process.env.REACT_APP_DEBUG === 'true' ? DebugApp : App;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppToRender />
   </React.StrictMode>
 );
